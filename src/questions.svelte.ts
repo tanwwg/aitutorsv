925a8f321c2ebd802295be1a984fa8dead6e56de
Tan Thor Jen <tan@wisewizardgames.com>
2025-02-06 16:46:07 +0800

    some work

---
+++ src/questions.svelte.ts
@@ -0,0 +1,99 @@
+
+import Handlebars from 'handlebars'
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+    apiKey: "9cae4f82f3814110bf94608105a4af135680729b8a4a207098bd76b9e04e3235",
+    baseURL: "https://api.together.xyz/v1",
+    dangerouslyAllowBrowser: true
+});
+
+type QuestionDisplay = {
+    question: string,
+    chatDisplay: OpenAI.Chat.Completions.ChatCompletionMessageParam[]
+}
+
+export const display: QuestionDisplay = $state({
+    question: "",
+    chatDisplay: []
+});
+
+enum QuestionMode {
+    WaitingForPrompt, Submitting
+}
+
+type QuestionState = {
+    json: any | null,
+    systemPromptFunc: HandlebarsTemplateDelegate<any> | null,
+
+    questionIndex: number,
+    mode: QuestionMode,
+
+    chatMessages: OpenAI.Chat.Completions.ChatCompletionMessageParam[]
+};
+
+var questionState: QuestionState = {
+    json: null,
+    systemPromptFunc: null,
+    questionIndex: 0,
+    mode: QuestionMode.WaitingForPrompt,
+
+    chatMessages: []
+};
+
+export async function nextQuestion() {
+    if (questionState.json == null) {
+        questionState.json = await (await fetch("/questions.json")).json();
+        questionState.systemPromptFunc = Handlebars.compile(questionState.json.system.prompt);
+    }
+    if (questionState.systemPromptFunc == null) return;
+
+    var question = questionState.json.questions[questionState.questionIndex];
+    display.question = question.display ?? question.question;
+
+    let systemPrompt = questionState.systemPromptFunc(question);
+    questionState.chatMessages = [{
+        "role": "system",
+        "content": systemPrompt
+    }];
+}   
+
+function setLastElement<T>(arr: T[], value: T) {
+    if (arr.length > 0) {
+        arr[arr.length - 1] = value;
+    } else {
+        arr.push(value);
+    }
+  }
+
+export async function sendPrompt(prompt: string, react: () => void) {
+    let userMessage: OpenAI.Chat.Completions.ChatCompletionMessageParam = {
+        role: "user",
+        content: prompt
+    };
+    questionState.chatMessages.push(userMessage);
+    display.chatDisplay.push(userMessage);
+    react();
+ 
+    const stream = await openai.chat.completions.create({
+        model: "meta-llama/Llama-3.3-70B-Instruct-Turbo",
+        messages: questionState.chatMessages,
+        stream: true,
+    });
+
+    let reply: OpenAI.Chat.Completions.ChatCompletionMessageParam = {
+        role: "assistant",
+        content: ""
+    };
+    questionState.chatMessages.push(reply);
+    display.chatDisplay.push(reply);
+    react();    
+    for await (const chunk of stream) {
+        let text = chunk.choices[0]?.delta?.content || '';
+        console.log(text);
+        reply.content += text;
+        setLastElement(questionState.chatMessages, reply);
+        setLastElement(display.chatDisplay, reply);
+        react();
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,82 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { setResponse } from '@sveltejs/kit/node';
+    import { display, nextQuestion, sendPrompt } from '../questions.svelte.js';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        nextQuestion();
+    })
+
+    let promptText: HTMLInputElement | undefined = $state();
+    let promptInput = $state("");
+
+    let promptButton: HTMLElement | undefined = $state();
+
+    function scrollToEnd() {
+        promptButton?.scrollIntoView({ behavior: 'smooth', block: 'end' });        
+    }
+
+    async function submitPrompt() {
+        let s = promptInput;
+        promptInput = "";       
+
+        await sendPrompt(s, scrollToEnd);
+
+        scrollToEnd();
+        promptText?.focus();
+    }
+
+    let aiText: HTMLDivElement | undefined = $state();
+
+    function onReturn(event: KeyboardEvent) {
+        if (event.key === 'Enter') {
+            submitPrompt();
+        }
+    }
+
+</script>
+
+<div class="container mt-5">
+    <div class="card p-4 shadow">
+        <h1 id="heading" class="mb-3">Question</h1>
+        <div id="question">
+            {display.question}
+        </div>
+    </div>
+
+    <div bind:this={aiText}>
+        {#each display.chatDisplay as item}
+            <br/>
+            <br/>
+            {#if item.role == "user"}
+                <div class='card p-3 text-end' style='background-color: #f0f0f0;'>
+                    <p>
+                        {item.content}
+                    </p>
+                </div>
+            {:else}                
+                <div class='p-3'>
+                    <p>
+                        {item.content}
+                    </p>
+                </div>
+            {/if}
+        {/each}
+    </div>
+
+    <div>
+        <div class="mt-3">
+            <input type="text" id="prompt-text" class="form-control" 
+                onkeydown={onReturn}
+                bind:this={promptText} 
+                bind:value={promptInput} 
+                placeholder="Prompt">
+        </div>
+        <button bind:this={promptButton} class="btn btn-primary mt-3" type="button" onclick={submitPrompt}>Submit</button>
+        <button id="start-listen" class="btn btn-secondary mt-3" type="button">🎤 Start Listening</button>
+        <button id="stop-listen" class="btn btn-danger mt-3" type="button" style="display: none;">Stop Listening</button>
+
+        <button id="next-question" class="btn btn-success mt-3 float-end" type="button">Next Question</button>				
+    </div>
+
+</div>
